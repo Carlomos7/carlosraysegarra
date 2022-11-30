d2e8e6ac32962ccb07a662fbf4980214a4e821d5
carlomos7 <cs525@njit.edu>
2022-11-30 17:59:50 -0500

    reworked html for navbar and updated styling

--- public_html/Portfolio/home.php
+++ public_html/Portfolio/home.php
@@ -3,4 +3,32 @@ require(__DIR__ . "/../../partials/nav.php");
 ?>
 <head>
     <Title>Carlos Ray Segarra</Title>
-<head>
+</head>
+<body>
+    <article class="d-flex flex-column min-vh-100 justify-content-center align-items-center" id="home" style="padding:128px 16px">
+        <h1 class="cs-jumbo"><b>Carlos Ray Segarra</b></h1>
+        <p>My name is Carlos Segarra. I am a third-year Information Technology major specializing in Website Development.
+        Most of my tech background comes from gaming. I attended a coding camp focused on Minecraft mod development
+        when I was around 13 and have been hooked ever since. In making this website, I am hoping to advance my skills
+        and learn new ones. </p>
+        <p>
+            <div class="container-fluid home-card-container">
+                <div class="card home-card">
+                  <img src="https://www.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png" class="card-img-top" alt="blank profile picture">
+                  <div class="card-body">
+                    <a href="<?php echo get_url('contactme.php'); ?>" class="btn">Contact Me</a>
+                  </div>
+                </div>
+            </div>
+        My hobbies include reading books and comics, playing video games, and coding. I have proficient Java
+        programming skills and general knowledge of HTML, MySQL, PHP, JavaScript, and C++. Lorem ipsum dolor sit amet,
+        consectetur adipiscing elit. Phasellus imperdiet, nulla et dictum interdum, nisi lorem egestas odio, vitae
+        scelerisque enim ligula venenatis dolor. Maecenas nisl est, ultrices nec congue eget, auctor vitae massa.
+        Fusce luctus vestibulum augue ut aliquet. Mauris ante ligula, facilisis sed ornare eu, lobortis in odio.
+        Praesent convallis urna a lacus interdum ut hendrerit risus congue. Nunc sagittis dictum nisi, sed ullamcorper
+        ipsum dignissim ac. In at libero sed nunc venenatis imperdiet sed ornare turpis. Donec vitae dui eget tellus
+        gravida venenatis. Integer fringilla congue eros non fermentum. Sed dapibus pulvinar nibh tempor porta. Cras
+        ac leo purus. Mauris quis diam velit</p>
+
+    </article>
+</body>
