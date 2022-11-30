9c3b1fd1bc842e670eded9aff0cbc62d01eedda4
carlomos7 <cs525@njit.edu>
2022-11-30 12:38:38 -0500

    fixed index.php redirect

--- public_html/index.php
+++ public_html/index.php
@@ -1,3 +1,6 @@
 <!--DEVELOPER: Carlos Segarra, IS 117-003, Fall 2022 -->
-<?php echo "Hello World!";
+<?php
+require(__DIR__ . "/../lib/functions.php");
+echo "Hello World!";
+redirect("$BASE_PATH" . "/../home.php");
 ?>
