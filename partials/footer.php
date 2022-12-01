fe1ab1fa584071198619e316197dcc8559c1c666
Carlomos7 <cs525@njit.edu>
2022-12-01 01:28:46 -0500

    added footer and styles

---
+++ partials/footer.php
@@ -0,0 +1,12 @@
+<?php
+require_once(__DIR__ . "/../lib/functions.php");
+
+?>
+<head>
+    <link rel="stylesheet" href="<?php echo get_url('styles/styles.css'); ?>">
+</head>
+<footer class="page-footer font-small mt-auto">
+  <div class="footer-copyright text-center py-3">© 2022 Copyright:
+    <a href="<?php echo get_url('home.php') ?>"> carlosray.xyz</a>
+  </div>
+</footer>
