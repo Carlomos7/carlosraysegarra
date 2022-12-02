cde156bd3d9bc77510972a0c3e2cb2c41c4176bf
Carlomos7 <cs525@njit.edu>
2022-12-01 20:09:05 -0500

    link fix

--- partials/nav.php
+++ partials/nav.php
@@ -1,14 +1,11 @@
 <!--DEVELOPER: Carlos Segarra, IS 117-003, Fall 2022 -->
 <?php
 require_once(__DIR__ . "/../lib/functions.php");
-//Note: this is to resolve cookie issues with port numbers
 $domain = $_SERVER["HTTP_HOST"];
 if (strpos($domain, ":")) {
     $domain = explode(":", $domain)[0];
 }
-$localWorks = true; //some people have issues with localhost for the cookie params
-//if you're one of those people make this false
-//this is an extra condition added to "resolve" the localhost issue for the session cookie
+$localWorks = true;
 if (($localWorks && $domain == "localhost") || $domain != "localhost") {
     session_set_cookie_params([
         "lifetime" => 60 * 60,
