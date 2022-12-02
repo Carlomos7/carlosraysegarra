0eb4fe4757e8fd8f422d4b7681d194876d380d93
Carlomos7 <cs525@njit.edu>
2022-12-02 00:36:30 -0500

    google analytics

--- partials/nav.php
+++ partials/nav.php
@@ -32,6 +32,7 @@ session_start();
 
     gtag('config', 'G-77TYX3MYNW');
   </script>
+
   <meta charset="UTF-8" name="viewport" content="width=device-width, intitial-scale=1">
   <link rel="icon" href="https://carlomos7.github.io/image-repo/favicon.ico">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
