a0ad1a9fed110614a599302cbf13ba9ac12cccc4
Carlomos7 <cs525@njit.edu>
2022-12-05 12:52:52 -0500

    testing mobile redirect fix

--- public_html/index.php
+++ public_html/index.php
@@ -1,9 +1,7 @@
 <!--DEVELOPER: Carlos Segarra, IS 117-003, Fall 2022 -->
 <?php
 require(__DIR__ . "/../lib/functions.php");
-echo "Hello World!";
-redirect("$BASE_PATH" . "/../../home.php");
-?>
+redirect("$BASE_PATH" . "/home.php");
 <head>
     <?php include_once("analyticstracking.php") ?>
 </head>
