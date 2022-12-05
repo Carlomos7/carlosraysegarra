3e387ce358d9565e830476dda5daff4305f3079c
Carlomos7 <cs525@njit.edu>
2022-12-05 13:10:43 -0500

    testing mobile redirect fix

--- public_html/index.php
+++ public_html/index.php
@@ -1,6 +1,7 @@
 <!--DEVELOPER: Carlos Segarra, IS 117-003, Fall 2022 -->
 <?php
 require(__DIR__ . "/../lib/functions.php");
+$BASE_PATH = '\Portfolio';
 echo "Hello World!";
 redirect("$BASE_PATH" . "/../../home.php");
 ?>
