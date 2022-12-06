a014852edad119cb2fb658d02b7ae24070ee5c4b
Carlomos7 <cs525@njit.edu>
2022-12-06 17:12:56 -0500

    resume rework

--- public_html/Portfolio/resume.php
+++ public_html/Portfolio/resume.php
@@ -8,104 +8,109 @@ require(__DIR__ . "/../../partials/nav.php");
 <head>
 <body>
     <article class="content-containers">
-    <div class="table-container">
+    <div class="container-fluid table-container justify-content-center">
 
-        <div class="row">
-            <div class ="column heading">
+        <div class="row table-heading">
+            <div class ="col-md-auto">
                 <h1>Carlos Segarra</h1>
             </div>
-            <div class="column contact">
+            <div class="col-sm-4">
                     114 Carlisle St.
                     <br>South Plainfield, NJ 07080
                     <br>(973) 951-2465
                     <br>carlos.segarrajf@gmail.com
             </div>
+            <hr>
         </div>
 
         <div class="row">
 
-        <div class="column">
-        <table >
-            <tr>
-                <td><h4>Education</h4>
-                BS, INFORMATION
-                TECHNOLOGY
-                New Jersey Institute of
-                Technology, Newark NJ
-                EXPECTED GRADUATION
-                DECEMBER 2023</td>
-            </tr>
-            <tr>
-                <td><h4>Skills</h4>
-                Python, Java, JavaScript,
-                HTML, CSS, PHP, SQL,
-                 GIT Docker, Flask
-                  Languages:
-                 <br>Fluent in Spanish
-                </td>
-            </tr>
-            <tr>
-                <td><h4>RELEVANT COURSES</h4>
-                    Computer Systems and
-                    Networks
-                    Internet & Applications
-                    Database Design
-                    Management & Applications
-                    Advanced Programming for
-                    Information Technology
-                    Information Design
-                    Techniques
-                </td>
-            </tr>
-        </table>
-        </div>
+            <div class="col-sm-3">
+            <table>
+                <tr>
+                    <td><h4>Education</h4>
+                    BS, INFORMATION
+                    TECHNOLOGY
+                    <br>New Jersey Institute of
+                    Technology, <br>Newark NJ
+                    <br>EXPECTED GRADUATION
+                    DECEMBER 2023<hr></td>
+                </tr>
+                <tr>
+                    <td><h4>Skills</h4>
+                    Python, Java, JavaScript,
+                    HTML, CSS, PHP, SQL,
+                     GIT Docker, Flask
+                      Languages:
+                     <br>Fluent in Spanish<hr>
+                    </td>
+                </tr>
+                <tr>
+                    <td><h4>RELEVANT COURSES</h4>
+                        Computer Systems and
+                        Networks
+                        Internet & Applications
+                        Database Design
+                        Management & Applications
+                        Advanced Programming for
+                        Information Technology
+                        Information Design
+                        Techniques
+                    </td>
+                </tr>
+            </table>
+            </div>
+            <div class= "col-md-auto vertical-line"></div>
+            <div class="col-sm">
+                <table >
+                    <tr>
+                    <td><h4>OBJECTIVE</h4>
+                        Seeking a part-time internship where I can utilize my
+                        understanding of and experience in core concepts related to
+                        website development and computer programming as a
+                        member of the information technology field.
+                        <hr>
+                    </td>
+                    </tr>
+                    <td><h4>WORK EXPERIENCE</h4>
+                        WHOLE FOODS | Metuchen, NJ | 2020 – 2022<br>
+                        <i>Grocery Team Member</i>
+                        <ul>
+                            <li>Cultivated a customer-focused shopping environment by
+                                greeting and responding to all customers in a friendly manne</li>
+                            <li>Developed knowledge about products and sales items to
+                                answer shoppers' questions</li>
+                            <li>Completed all point of sale opening and closing procedures</li>
+                        </ul>
+                        NEW JERSEY INNOVATION INSTITUTE | Newark, NJ | 2018 – 2020<br>
+                        <i>Project Assistant</i>
+                        <ul>
+                            <li>Assisted in general administrative support on various projects</li>
+                            <li>Maintained legal documents, such as participation and
+                                business associate agreements</li>
+                            <li>Exercised graphic design skills to conceptualize flyers, badges,
+                                and other conference event materials</li>
+                            <li>Compiled various reports and spreadsheets, such as team
+                                projections and quarterly invoices</li>
+                            <li>Scheduled and coordinated meeting appointments</li>
+                            <li>Transcribed meeting minutes and distributed via email</li>
+                            <li>Maintained utmost discretion when dealing with PHI</li>
+                            <li>Trained new project assistants</li>
+                        </ul>
+                    </td>
+                    <tr>
+                    </tr>
+                </table>
+            </div>
 
-        <div class="column">
-        <table >
-            <tr>
-            <td><h4>OBJECTIVE</h4>
-                Seeking a part-time internship where I can utilize my
-                understanding of and experience in core concepts related to
-                website development and computer programming as a
-                member of the information technology field.
-            </td>
-            </tr>
-            <td><h4>WORK EXPERIENCE</h4>
-                WHOLE FOODS | Metuchen, NJ | 2020 – 2022<br>
-                <i>Grocery Team Member</i>
-                <ul>
-                    <li>Cultivated a customer-focused shopping environment by
-                        greeting and responding to all customers in a friendly manne</li>
-                    <li>Developed knowledge about products and sales items to
-                        answer shoppers' questions</li>
-                    <li>Completed all point of sale opening and closing procedures</li>
-                </ul>
-                NEW JERSEY INNOVATION INSTITUTE | Newark, NJ | 2018 – 2020<br>
-                <i>Project Assistant</i>
-                <ul>
-                    <li>Assisted in general administrative support on various projects</li>
-                    <li>Maintained legal documents, such as participation and
-                        business associate agreements</li>
-                    <li>Exercised graphic design skills to conceptualize flyers, badges,
-                        and other conference event materials</li>
-                    <li>Compiled various reports and spreadsheets, such as team
-                        projections and quarterly invoices</li>
-                    <li>Scheduled and coordinated meeting appointments</li>
-                    <li>Transcribed meeting minutes and distributed via email</li>
-                    <li>Maintained utmost discretion when dealing with PHI</li>
-                    <li>Trained new project assistants</li>
-                </ul>
-            </td>
-            <tr>
-            </tr>
-        </table>
         </div>
-
-        </div>
+        
     </div>
-
-        <a href="files\Carlos_Segarra_2022.pdf" class="btn mt-auto align-self-start" download>Download Resumé!</a>
-        <a href="https://carlomos7.github.io/image-repo/Carlos_Segarra_2022.pdf" class="btn mt-auto align-self-start" target="_blank" rel="noopener noreferrer">View Resumé!</a>
+        <div class="container-fluid d-flex justify-content-center" style="padding:1rem 0 1rem 0;">
+        <a href="files\Carlos_Segarra_2022.pdf" class="btn mt-auto align-self-start" download>Download PDF</a>
+        <a href="https://carlomos7.github.io/image-repo/Carlos_Segarra_2022.pdf" class="btn mt-auto align-self-start"
+        target="_blank" rel="noopener noreferrer">View PDF</a>
+        </div>
     </article>
     <?php include_once(__DIR__ . "/../../partials/footer.php") ?>
 </body>
