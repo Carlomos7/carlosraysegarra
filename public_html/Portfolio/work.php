bf60cd7f25a4a4b3bb035e10094f2570d8676564
Carlos Ray <106279547+Carlomos7@users.noreply.github.com>
2023-01-16 16:46:21 -0500

    Update work.php

--- public_html/Portfolio/work.php
+++ public_html/Portfolio/work.php
@@ -11,7 +11,7 @@ require(__DIR__ . "/../../partials/nav.php");
         <h1><b>My Work Experience</b></h1>
         <hr>
         <h2><strong> New Jersey Innovation Institute </strong>
-        2018 to 2022 <h3> <i>Project Assistant</i> </h3>
+        2018 to 2020 <h3> <i>Project Assistant</i> </h3>
         </h2>
         <p>At New Jersey Innovation Institute, I refined my skills in organizing and maintaining projects. I was a
         part of the Promoting Interoperability team under NJII's healthcare division. The PI Team oversaw the
@@ -20,16 +20,6 @@ require(__DIR__ . "/../../partials/nav.php");
         hardware. As such, my experience there consisted of working collaboratively on specific short- and long-term
          projects. I oversaw tasks delegated to me by my project manager, coordinated with all necessary vendors and
           service providers, and ensured that projects were moving forward on time.</p>
-        <h2><strong> Job Placeholder </strong>
-        2018 to 2022 <h3> <i>Position</i> </h3>
-        </h2>
-        <p>Viverra suspendisse potenti nullam ac tortor vitae. Porttitor massa id neque aliquam. Auctor elit sed
-        vulputate mi sit amet mauris commodo. Egestas sed tempus urna et pharetra pharetra massa massa. Venenatis a
-        condimentum vitae sapien pellentesque habitant morbi tristique senectus. Diam ut venenatis tellus in metus
-        vulputate. Congue mauris rhoncus aenean vel elit scelerisque mauris pellentesque pulvinar. Sed libero enim sed
-        faucibus turpis in eu mi bibendum. Pulvinar elementum integer enim neque volutpat ac. Placerat vestibulum lectus
-         mauris ultrices. Eget est lorem ipsum dolor sit amet consectetur adipiscing elit. Lorem ipsum dolor sit amet
-         consectetur. Tristique senectus et netus et malesuada fames ac.</p>
 
          <a href="<?php echo get_url('resume.php'); ?>" class="btn mt-auto align-self-start"> Check out my Resumé!</a>
 
